e1989856fe24637dc13b96460d09f9e26447ff35
nortay250 <eleanortay97@gmail.com>
2021-11-24 10:13:46 +0800

    update our meni-otems to navigate with Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
